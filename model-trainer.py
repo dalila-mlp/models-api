296919aae7d34f262d0d3215abcd0ef06ee57a5f
SuPeR GB <ddegaugue@myges.fr>
2024-06-14 16:40:49 +0200

    modif model_trainer -> ajout classe abstraite

--- model-trainer.py
+++ model-trainer.py
@@ -2,7 +2,8 @@ import numpy as np
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'models-draft')))
-from classification_models.Tf_PMCClassifier_Dalila import PMCClassifier
+from classeAbstraite import DynamicParams
+from classification_models.XGBoost_Dalila import XGBoostClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score, roc_curve, precision_recall_curve, average_precision_score, log_loss, jaccard_score, cohen_kappa_score, matthews_corrcoef
 from tensorflow.keras.losses import CategoricalCrossentropy
 
@@ -63,8 +64,23 @@ class ModelTrainer_Sk:
     def __init__(self, classifier):
         self.classifier = classifier
 
-    def train(self, x_train, y_train,  x_val=None, y_val=None):
+    def validate_input_shape(self, x):
+        if hasattr(self.classifier, 'model') and hasattr(self.classifier.model, 'input_shape'):
+            expected_shape = self.classifier.model.input_shape
+            if x.shape[1:] != expected_shape[1:]:
+                raise ValueError("La dimension d'entrée {x.shape[1:]} n'est pas compatible avec la dimension attendue {expected_shape[1:]}")
+            else:
+                print("Shape validation not applicable for non-Keras models")
+
+    def train(self, x_train, y_train, x_val=None, y_val=None):
+        self.validate_input_shape(x_train)
+
+        y_train = np.argmax(y_train, axis=1)
+        if y_val is not None:
+            y_val = np.argmax(y_val, axis=1)
+
         if x_val is not None and y_val is not None:
+            self.validate_input_shape(x_val)
             self.classifier.train(x_train, y_train, validation_data=(x_val, y_val))
         else:
             self.classifier.train(x_train, y_train)
@@ -72,9 +88,9 @@ class ModelTrainer_Sk:
     def evaluate(self, x_test, y_test):
         # Obtenez les prédictions du modèle
         y_pred = self.classifier.predict(x_test)
-        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_pred_classes = y_pred
         y_pred_proba = np.eye(3)[y_pred_classes]
-        y_test_classes = np.argmax(y_test, axis=1)
+        y_test_classes = np.argmax(y_test, axis=1) if len(y_test.shape) > 1 and y_test.shape[1] > 1 else y_test
 
         metrics = {
             'accuracy': accuracy_score(y_test_classes, y_pred_classes),
@@ -107,11 +123,11 @@ class ModelTrainer_other:
     def __init__(self, classifier):
         self.classifier = classifier
 
-    def train(self, x_train, y_train, epochs=10, batch_size=32,  x_val=None, y_val=None):
+    def train(self, x_train, y_train, x_val=None, y_val=None):
         if x_val is not None and y_val is not None:
             self.classifier.train(x_train, y_train, validation_data=(x_val, y_val))
         else:
-            self.classifier.train(x_train, y_train, epochs, batch_size)
+            self.classifier.train(x_train, y_train)
 
     def evaluate(self, x_test, y_test):
         # Obtenez les prédictions du modèle
@@ -161,12 +177,13 @@ if __name__ == "__main__":
 
     from tensorflow.keras.utils import to_categorical
 
-    # Convertir les étiquettes de classe en format one-hot encoding
+    # # Convertir les étiquettes de classe en format one-hot encoding
     y_train = to_categorical(y_train, 3)
     y_test = to_categorical(y_test, 3)
 
     # Créer une instance du modèle XGBoostClassifier
-    model = PMCClassifier()
+    params = DynamicParams()
+    model = XGBoostClassifier(params)
 
     type = model.getTypeModel()
 
